c030697d2b257bd9df71e32ef65938f0daf55ea7
davidthomasparks <Davidthomasparks@gmail.com>
2023-05-03 11:26:39 -0700

    Marketing banner for launch special

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -13,6 +13,12 @@ export default async function MarketingLayout({
     return (
         <>
             <div className="flex min-h-screen flex-col">
+                <div className="w-full flex justify-center items-center py-2 px-4 text-center bg-primary-foreground">
+                    <span className="text-sm">
+                        🎉 Limited time launch special! Enter code{" "}
+                        <strong>LAUNCH</strong> for 50% off platform credits.
+                    </span>
+                </div>
                 <header className="container z-40 bg-background">
                     <div className="flex h-20 items-center justify-between py-6">
                         <MainNav items={marketingConfig.mainNav} />
