eef804317678fb9efc2f51d61f6c921201ca44db
davidthomasparks <Davidthomasparks@gmail.com>
2023-05-05 21:57:51 -0700

    Ship skill art, no examples yet

--- lib/generators.ts
+++ lib/generators.ts
@@ -5,6 +5,7 @@ export type TScenarioModels =
     // | "shields"
     // | "16x16Icons"
     | "pixelPortrait"
+    | "skillArt"
 
 type TScenarioModelsToIds = {
     [key in TScenarioModels]: string
@@ -17,6 +18,7 @@ export const scenarioGenerators: TScenarioModelsToIds = {
     // shields: "TtjEG2UmQQSxwIvlMh6p5A",
     // "16x16Icons": "aoEUJBxQQOejv_DA7rq-2g",
     pixelPortrait: "Zk4dmsnVQJ6x02F9IZfXhA",
+    skillArt: "VB8yd1LaRsinkQMZuFoupg",
 }
 
 type TNormalizedGeneratorNameMap = {
@@ -30,6 +32,7 @@ export const normalizedGeneratorMap = {
     TtjEG2UmQQSxwIvlMh6p5A: "32x32 Shields",
     "aoEUJBxQQOejv_DA7rq-2g": "16x16 Icons",
     Zk4dmsnVQJ6x02F9IZfXhA: "16x16 Pixel Portrait",
+    VB8yd1LaRsinkQMZuFoupg: "32x32 Skill Art",
 }
 
 export const supplementalPromptMap = {
@@ -39,16 +42,19 @@ export const supplementalPromptMap = {
     TtjEG2UmQQSxwIvlMh6p5A: "game icon, shield, 32x32, ",
     "aoEUJBxQQOejv_DA7rq-2g": "game icon, ",
     Zk4dmsnVQJ6x02F9IZfXhA: "game icon, 16x16, pixel art, close up, portrait, ",
+    VB8yd1LaRsinkQMZuFoupg: "game icon, ",
 }
 
 export const sizeLockedGenerators = [
     // scenarioGenerators["16x16Icons"],
     // scenarioGenerators.shields,
     scenarioGenerators.pixelPortrait,
+    scenarioGenerators.skillArt,
 ]
 
 export const sizeLockedGeneratorsSizeValue = {
     [scenarioGenerators.pixelPortrait]: 32,
+    [scenarioGenerators.skillArt]: 16,
     // [scenarioGenerators["16x16Icons"]]: 32,
     // [scenarioGenerators.shields]: 16,
 }
