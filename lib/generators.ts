6fa3935cd4de14e87b973ed5f6488088390252a4
davidthomasparks <Davidthomasparks@gmail.com>
2023-05-08 21:45:46 -0700

    Revert pixel art model

--- lib/generators.ts
+++ lib/generators.ts
@@ -2,8 +2,6 @@ export type TScenarioModels =
     | "fantasyRpg"
     | "landscapePortrait"
     | "animeStyle"
-    // | "shields"
-    // | "16x16Icons"
     | "pixelPortrait"
     | "skillArt"
 
@@ -15,9 +13,7 @@ export const scenarioGenerators: TScenarioModelsToIds = {
     fantasyRpg: "DkCC2BfCQ8mhxnyFW1tXcw",
     landscapePortrait: "BnhDXzIrQxWk1c0bWe73_w",
     animeStyle: "PNqixjKURbiouk49_gYWCw",
-    // shields: "TtjEG2UmQQSxwIvlMh6p5A",
-    // "16x16Icons": "aoEUJBxQQOejv_DA7rq-2g",
-    pixelPortrait: "eaJqvpaqQGCzRM1KiG5l7w",
+    pixelPortrait: "Zk4dmsnVQJ6x02F9IZfXhA",
     skillArt: "VB8yd1LaRsinkQMZuFoupg",
 }
 
@@ -29,9 +25,7 @@ export const normalizedGeneratorMap = {
     DkCC2BfCQ8mhxnyFW1tXcw: "Fantasy RPG",
     BnhDXzIrQxWk1c0bWe73_w: "Landscape Portrait",
     PNqixjKURbiouk49_gYWCw: "Anime Style",
-    TtjEG2UmQQSxwIvlMh6p5A: "32x32 Shields",
-    "aoEUJBxQQOejv_DA7rq-2g": "16x16 Icons",
-    eaJqvpaqQGCzRM1KiG5l7w: "16x16 Pixel Portrait",
+    Zk4dmsnVQJ6x02F9IZfXhA: "16x16 Pixel Portrait",
     VB8yd1LaRsinkQMZuFoupg: "32x32 Skill Art",
 }
 
@@ -39,11 +33,10 @@ export const supplementalPromptMap = {
     DkCC2BfCQ8mhxnyFW1tXcw: " , fantasy RPG style, concept art",
     BnhDXzIrQxWk1c0bWe73_w: " , landscape portrait, ",
     PNqixjKURbiouk49_gYWCw: " , anime style",
-    TtjEG2UmQQSxwIvlMh6p5A: " , game icon, shield, 32x32",
-    "aoEUJBxQQOejv_DA7rq-2g": " ,game icon",
-    eaJqvpaqQGCzRM1KiG5l7w: ", 16x16 pixel art, game icon",
+    Zk4dmsnVQJ6x02F9IZfXhA:
+        " , game icon, 16x16, pixel art, close up, portrait",
     VB8yd1LaRsinkQMZuFoupg:
-        " ,game icon, 32x32 pixel art, sRGB, skill art, close up, portrait",
+        " , game icon, 32x32 pixel art, sRGB, skill art, close up, portrait",
 }
 
 export const sizeLockedGenerators = [
