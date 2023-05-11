f16379322412626cb67ac516885839050e1fe610
davidthomasparks <Davidthomasparks@gmail.com>
2023-05-10 21:43:02 -0700

    Remove fantasy RPG entirely from fantasy RPG

--- lib/generators.ts
+++ lib/generators.ts
@@ -194,7 +194,7 @@ export const normalizedGeneratorMap = {
 }
 
 export const supplementalPromptMap = {
-    DkCC2BfCQ8mhxnyFW1tXcw: " , fantasy RPG",
+    DkCC2BfCQ8mhxnyFW1tXcw: "",
     BnhDXzIrQxWk1c0bWe73_w: " , landscape portrait, ",
     PNqixjKURbiouk49_gYWCw: " , anime style",
     Zk4dmsnVQJ6x02F9IZfXhA:
