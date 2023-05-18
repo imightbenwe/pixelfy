01aceeb75aa70c669b258db10c12d0fa57958a33
davidthomasparks <Davidthomasparks@gmail.com>
2023-05-18 08:55:51 -0700

    Fix credit layout from render blocking async calls that are unnecessary

--- app/(credits)/credits/layout.tsx
+++ app/(credits)/credits/layout.tsx
@@ -2,9 +2,6 @@ import { MainNav } from "@/components/main-nav"
 import { SiteFooter } from "@/components/site-footer"
 import { UserAccountNav } from "@/components/user-account-nav"
 import { dashboardConfig } from "@/config/dashboard"
-import { db } from "@/lib/db"
-import { getCurrentUser } from "@/lib/session"
-import { notFound } from "next/navigation"
 
 interface DashboardLayoutProps {
     children?: React.ReactNode
@@ -13,34 +10,12 @@ interface DashboardLayoutProps {
 export default async function DashboardLayout({
     children,
 }: DashboardLayoutProps) {
-    const user = await getCurrentUser()
-
-    if (!user) {
-        return notFound()
-    }
-
-    const userCredits = await db.user.findUnique({
-        where: {
-            id: user.id,
-        },
-        select: {
-            credits: true,
-        },
-    })
-
     return (
         <div className="flex min-h-screen flex-col space-y-6">
             <header className="sticky top-0 z-40 border-b bg-background">
                 <div className="container flex h-16 items-center justify-between py-4">
                     <MainNav items={dashboardConfig.mainNav} />
-                    <UserAccountNav
-                        user={{
-                            name: user.name,
-                            image: user.image,
-                            email: user.email,
-                        }}
-                        credits={userCredits?.credits || 0}
-                    />
+                    <UserAccountNav />
                 </div>
             </header>
             <div className="container grid flex-1 gap-12 pb-24">
