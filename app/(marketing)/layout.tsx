4455006e6430d325fb468280ba5342a1889faf2c
imightbenwe <koenw77@gmail.com>
2025-09-19 14:08:31 +0200

    Update layout.tsx

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -3,27 +3,28 @@ import { MainNav } from "@/components/main-nav"
 import { SiteFooter } from "@/components/site-footer"
 import { marketingConfig } from "@/config/marketing"
 
+export const dynamic = "force-dynamic"
+export const revalidate = 0
+
 interface MarketingLayoutProps {
-    children: React.ReactNode
+  children: React.ReactNode
 }
 
 export default async function MarketingLayout({
-    children,
+  children,
 }: MarketingLayoutProps) {
-    return (
-        <>
-            <div className="flex min-h-screen flex-col">
-                <header className="container z-40 bg-background">
-                    <div className="flex h-20 items-center justify-between py-6">
-                        <MainNav items={marketingConfig.mainNav} />
-                        <nav>
-                            <LoginButton />
-                        </nav>
-                    </div>
-                </header>
-                <main className="flex-1">{children}</main>
-                <SiteFooter />
-            </div>
-        </>
-    )
+  return (
+    <div className="flex min-h-screen flex-col">
+      <header className="container z-40 bg-background">
+        <div className="flex h-20 items-center justify-between py-6">
+          <MainNav items={marketingConfig.mainNav} />
+          <nav>
+            <LoginButton />
+          </nav>
+        </div>
+      </header>
+      <main className="flex-1">{children}</main>
+      <SiteFooter />
+    </div>
+  )
 }
