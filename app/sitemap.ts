b3baed96393fdec342447ec212ff9ac3a63d17f9
imightbenwe <koenw77@gmail.com>
2025-09-19 14:14:22 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,43 +1,47 @@
+// app/sitemap.ts
 import { db } from "@/lib/db"
 import { scenarioModelData } from "@/lib/generators"
-import { MetadataRoute } from "next"
+import type { MetadataRoute } from "next"
+
+export const dynamic = "force-dynamic"
+export const revalidate = 0
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const images = await db.outputImage.findMany({
-        select: {
-            id: true,
-            updatedAt: true,
-        },
-    })
+  const images = await db.outputImage.findMany({
+    select: {
+      id: true,
+      updatedAt: true,
+    },
+  })
 
-    const examplesSitemapEntry = Object.keys(scenarioModelData).map((style) => {
-        return {
-            url: `https://pixelfy.ai/examples/${scenarioModelData[style].slug}`,
-            lastModified: new Date(),
-        }
-    })
+  const examplesSitemapEntry = Object.keys(scenarioModelData).map((style) => {
+    return {
+      url: `https://pixelfy.ai/examples/${scenarioModelData[style].slug}`,
+      lastModified: new Date(),
+    }
+  })
 
-    const imageSitemapEntry = images.map((image) => {
-        return {
-            url: `https://pixelfy.ai/i/${image.id}`,
-            lastModified: image.updatedAt,
-        }
-    })
+  const imageSitemapEntry = images.map((image) => {
+    return {
+      url: `https://pixelfy.ai/i/${image.id}`,
+      lastModified: image.updatedAt,
+    }
+  })
 
-    return [
-        {
-            url: "https://pixelfy.ai",
-            lastModified: new Date(),
-        },
-        {
-            url: "https://pixelfy.ai/tos",
-            lastModified: new Date(),
-        },
-        {
-            url: "https://pixelfy.ai/privacy-policy",
-            lastModified: new Date(),
-        },
-        ...examplesSitemapEntry,
-        ...imageSitemapEntry,
-    ]
+  return [
+    {
+      url: "https://pixelfy.ai",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://pixelfy.ai/tos",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://pixelfy.ai/privacy-policy",
+      lastModified: new Date(),
+    },
+    ...examplesSitemapEntry,
+    ...imageSitemapEntry,
+  ]
 }
